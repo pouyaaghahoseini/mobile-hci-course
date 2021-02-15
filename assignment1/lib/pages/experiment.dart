1a20540e5d7d2de6b68b6dddb3053b691c26765b
pouyaaghahoseini <pouyaaghahoseini@gmail.com>
2021-02-14 20:57:57 -0600

    add findTarget

--- assignment1/lib/pages/experiment.dart
+++ assignment1/lib/pages/experiment.dart
@@ -1,43 +1,63 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
+List findTarget(int r, double x, double y) {
+  double top = 1, left = 1;
+  double angle = Random().nextDouble() * 2 * pi;
+  top = x + r * cos(angle);
+  left = y + r * sin(angle);
+  if (top < 832 && left < 600) {
+    return [top, left];
+  } else {
+    return findTarget(r, x, y);
+  }
+}
+
 class Experiment extends StatefulWidget {
   @override
   _ExperimentState createState() => _ExperimentState();
 }
 
 class _ExperimentState extends State<Experiment> {
-  // double topRandom, leftRandom;
   double topRandom = Random().nextInt(350).toDouble() + 250.0;
   double leftRandom = Random().nextInt(320).toDouble();
-
+  List Target = [100, 100];
+  int width = 50;
+  int dist = 200;
   @override
   void initState() {
-    print("initstate function ran");
+    print("initstate function ran------------");
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    print("build function ran");
+    print("build function ran----");
     return Scaffold(
       appBar: AppBar(
         title: Text("Experiment Started!"),
       ),
-      body: Stack(
-        children: [
-          newPoint(),
-          Positioned(
-            child: TargetPoint(200),
-            top: 100,
-            left: 50,
-          )
-        ],
+      body: InkWell(
+        onTap: () {
+          print("Stack Tapped!!!!");
+        },
+        child: Stack(
+          children: [
+            newStartPoint(),
+            Positioned(
+              child: TargetPoint(200),
+              top: 100,
+              left: 50,
+            )
+          ],
+        ),
       ),
     );
   }
 
-  Widget newPoint() => Positioned(
+  Widget newStartPoint() => Positioned(
+        height: 100,
+        width: 100,
         child: FloatingActionButton(
           backgroundColor: Colors.amber,
           child: Text("A"),
@@ -45,8 +65,10 @@ class _ExperimentState extends State<Experiment> {
             setState(() {
               topRandom = Random().nextInt(350).toDouble() + 250.0;
               leftRandom = Random().nextInt(320).toDouble();
-              print("topRandom is $topRandom");
-              print("leftRandom is $leftRandom");
+              Target = findTarget(100, topRandom, leftRandom);
+              // print("topRandom is $topRandom");
+              // print("leftRandom is $leftRandom");
+              print("Target is on $Target");
             });
           },
         ),
