da2ce041f7a0287433fbf20013ea6c8ad1ddb31b
pouyaaghahoseini <pouyaaghahoseini@gmail.com>
2021-02-14 22:22:55 -0600

    Finish homepage

--- assignment1/lib/pages/home.dart
+++ assignment1/lib/pages/home.dart
@@ -10,20 +10,63 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff15202b),
       appBar: AppBar(
         title: Text('Welcome to Fitts Law Experiment'),
+        centerTitle: true,
+        backgroundColor: Color(0xff1D2C3B),
       ),
-      body: Center(
-          child: Text(
-        'Please use your index finger to hit the buttons\n First select the start then the target \n Hit GO to start, Thanks!',
-        style: TextStyle(fontSize: 15),
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/experiment');
-        },
-        child: Text('GO'),
-      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              'Please choose your input method.',
+              style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  height: 100.0,
+                  width: 150.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/experiment');
+                    },
+                    child: const Text(
+                      "Thumb",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 100.0,
+                  width: 200.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/experiment');
+                    },
+                    child: const Text(
+                      "Index Finger",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ]),
     );
   }
 }
