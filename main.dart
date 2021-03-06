5827dd2dfa1a2cf6ab7008e4736cb5e2a942f410
Afsane-Baghestani <78616851+Afsane-Baghestani@users.noreply.github.com>
2021-03-06 22:32:16 +0330

    Add files via upload

--- main.dart
+++ main.dart
@@ -29,15 +29,19 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Offset> points = <Offset>[];
+  Offset
+      firstPoint; //for remembering the first point of the line (for line mode)
+  String mode =
+      "FreeDraw"; //for passing the mode selected from popup menu to others
 
   @override
   Widget build(BuildContext context) {
     final Container sketchArea = Container(
       margin: EdgeInsets.all(4.0),
       alignment: Alignment.topLeft,
-      color: Colors.blueGrey[50],
+      color: Colors.blueGrey[50], //background color of the paint area
       child: CustomPaint(
-        painter: Sketcher(points),
+        painter: Sketcher(points, mode),
       ),
     );
 
@@ -59,13 +63,22 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: GestureDetector(
+        onPanDown: (DragDownDetails details) {
+          setState(() {
+            RenderBox box2 = context.findRenderObject(); //finds the scaffold
+            Offset point2 = box2.globalToLocal(details.globalPosition);
+            point2 =
+                point2.translate(0.0, -(AppBar().preferredSize.height + 30));
+            firstPoint = point2; //for the first point of the line mode
+            points = List.from(points)..add(point2);
+          });
+        },
         onPanUpdate: (DragUpdateDetails details) {
           //when the user touch the screen and move
           setState(() {
             RenderBox box = context.findRenderObject(); //finds the scaffold
             Offset point = box.globalToLocal(details.globalPosition);
             point = point.translate(0.0, -(AppBar().preferredSize.height + 30));
-            //reduce the status bar and appbar height from it (the "30" might be different in your device)
 
             points = List.from(points)
               ..add(point); //add the points when user drag in screen
@@ -73,8 +86,21 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         onPanEnd: (DragEndDetails details) {
           setState(() {
-            points.add(null);
-            //points.clear();
+            if (mode == "Line") {
+              Offset lastPoint =
+                  points.last; //storing the last point for drawing the line
+              points = List.from(points)..add(firstPoint);
+              points = List.from(points)..add(lastPoint);
+              points.add(null);
+              int firstInx = List.from(points).indexOf(firstPoint);
+              int lastindx = List.from(points).lastIndexOf(firstPoint);
+
+              points.removeRange(
+                  firstInx, lastindx); //erasing the line that use drew
+
+            } else if (mode == "FreeDraw") {
+              points.add(null);
+            }
           });
         },
         child: sketchArea,
@@ -98,30 +124,34 @@ class _MyHomePageState extends State<MyHomePage> {
     switch (choice) {
       case Constants.freeDraw:
         {
+          mode = "FreeDraw";
           print("#### Free Draw has been selected ####");
-          //points.clear();
         }
         break;
 
       case Constants.line:
         {
+          mode = "Line";
           print("#### Line has been selected ####");
         }
         break;
 
       case Constants.circle:
         {
+          mode = "Circle";
           print("#### Circle has been selected ####");
         }
         break;
 
       case Constants.rectangle:
         {
+          mode = "Rectangle";
           print("#### Rectangle has been selected ####");
         }
         break;
       case Constants.triangle:
         {
+          mode = "Triangle";
           print("#### Triangle has been selected ####");
         }
         break;
@@ -137,7 +167,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class Sketcher extends CustomPainter {
   final List<Offset> points;
-  Sketcher(this.points);
+  String mode;
+  Sketcher(this.points, this.mode);
 
   @override
   bool shouldRepaint(Sketcher oldDelegate) {
@@ -150,10 +181,41 @@ class Sketcher extends CustomPainter {
       ..strokeCap = StrokeCap.round //the shape of a single dot (single touch)
       ..strokeWidth = 4.0; // the width of a single dot (single touch)
 
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != null && points[i + 1] != null) {
-        canvas.drawLine(points[i], points[i + 1], paint);
-      }
+    switch (mode) {
+      case "FreeDraw":
+        {
+          for (int i = 0; i < points.length - 1; i++) {
+            if (points[i] != null && points[i + 1] != null) {
+              canvas.drawLine(points[i], points[i + 1], paint);
+            }
+          }
+        }
+        break;
+      case "Line":
+        {
+          for (int i = 0; i < points.length - 1; i++) {
+            if (points[i] != null && points[i + 1] != null) {
+              canvas.drawLine(points[i], points[i + 1], paint);
+            }
+          }
+        }
+        break;
+
+      case "Circle":
+        {}
+        break;
+
+      case "Rectangle":
+        {}
+        break;
+      case "Triangle":
+        {}
+        break;
+      default:
+        {
+          //statements;
+        }
+        break;
     }
   }
 }
